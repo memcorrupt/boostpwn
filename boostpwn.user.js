0aada24cef264db1d848eacd1c4cd1fc88d8eaca
memcorrupt <41022155+memcorrupt@users.noreply.github.com>
2021-04-24 18:42:31 -0400

    Update boostpwn.user.js

--- boostpwn.user.js
+++ boostpwn.user.js
@@ -1,13 +1,35 @@
 // ==UserScript==
 // @name         BoostPwn
-// @namespace    https://liquidram.tech/
-// @version      0.1
+// @namespace    https://mem.rip/
+// @version      1.0
 // @description  dont do the verification things on boost.ink
 // @author       memcorrupt
 // @match        https://boost.ink/*
 // @grant        none
-// @run-at document-end
+// @run-at document-start
 // ==/UserScript==
 
-$(".complete_btn").removeAttr("disabled")[0].onclick=function(){location.href=atob(atob(jQuery(this).attr("data-href")));};
-$ = null;
+(function(){
+    'use strict';
+    const observer = new MutationObserver(mutations => {
+        for(const mutation of mutations){
+            for(const node of mutation.addedNodes){
+                if(node.src && node.src.match("unlock.js")){
+                    let url = node.getAttribute("version");
+                    if(!url){
+                        alert("boostpwn is broken");
+                        return;
+                    }
+                    observer.disconnect();
+                    setTimeout(() => { //chrome bug causes access violation ?
+                        location.href = atob(url);
+                    });
+                }
+            }
+        }
+    });
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+})();
